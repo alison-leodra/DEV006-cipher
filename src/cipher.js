4afb456310a4c8681835ad31beb7231dc29c95d2
alison=leodra <alison.le.saa@gmail.com>
2023-03-13 12:51:01 -0300

    addEventListener funcionando correctamente, desarrollo funciones

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,15 @@
 const cipher = {
-  // ...
-};
+  
+ encode: (param1)=> {
+  
+  return alert(param1.value)
+ },
+
+ decode: (param1)=>{
+  
+  return alert(param1.value);
+ }
+
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import cipher from './cipher.js';
 
+const encodeButton = document.getElementById("encodeButton");
+const encodeText = document.getElementById("encodeText");
+const decodeButton = document.getElementById("decodeButton");
+const decodeText = document.getElementById("decodeText");
+
+
+encodeButton.addEventListener("click",cipher.encode(encodeText));
+decodeButton.addEventListener("click",cipher.decode(decodeText));
+
+
 console.log(cipher);
