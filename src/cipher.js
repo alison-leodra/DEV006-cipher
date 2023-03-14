67fbdf9f849d553faeafdee0fd755816b47933ac
alison=leodra <alison.le.saa@gmail.com>
2023-03-14 12:39:06 -0300

    Encode funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -3,7 +3,32 @@ const cipher = {
  encode: (param1,param2)=> {
     const toEncode = param1;
     const offsetNumber1 = param2;
-    console.debug(`esta funcionando : ${toEncode}, ${offsetNumber1}` )
+    let charsNum = toEncode.length;
+    let encodeFinal="";
+
+    for (let i=0 ; i < charsNum ; i++)
+    {
+
+    let ascciNum = toEncode.charCodeAt(i);
+    let newNum = (parseInt(ascciNum) + parseInt(offsetNumber1))
+
+    if (parseInt(ascciNum)!= 32 ){
+
+      if (newNum > 90){
+      let newChar = String.fromCharCode(((newNum-65)%26)+65);
+      encodeFinal = encodeFinal + newChar;
+      }
+      else{
+      let newChar = String.fromCharCode(newNum);
+      encodeFinal = encodeFinal + newChar;
+      }
+    }
+    else{
+      encodeFinal = encodeFinal + " ";
+    }
+   }
+   console.log(encodeFinal)
+   alert(encodeFinal);
  },
 
  decode: (param1,param2)=>{
